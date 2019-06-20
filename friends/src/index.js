d91f9f6a4e0805692b3882ac81ddd8be784f8654
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-20 13:44:31 +0100

    Created friendcard list

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import { Provider } from "react-redux"
 import ReactDOM from 'react-dom';
+import rootReducer from "./Redux/reducers"
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
